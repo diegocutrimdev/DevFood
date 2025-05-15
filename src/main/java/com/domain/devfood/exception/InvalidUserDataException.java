1c4c04b5b21c058721ba4375f7ac35e9ac65520c
dicutrim <diegocutrimcontato@gmail.com>
2025-05-15 14:28:34 -0300

    Implementação do UserRepository e das exceções de validação

--- src/main/java/com/domain/devfood/exception/InvalidUserDataException.java
+++ src/main/java/com/domain/devfood/exception/InvalidUserDataException.java
@@ -1,7 +1,7 @@
 package com.domain.devfood.exception;
 
 public class InvalidUserDataException extends RuntimeException {
-  public InvalidUserDataException(String message) {
-    super(message);
-  }
+    public InvalidUserDataException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/domain/devfood/repository/UserRepository.java
+++ src/main/java/com/domain/devfood/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.domain.devfood.repository;
 
-public class UserRepository {
+import com.domain.devfood.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
