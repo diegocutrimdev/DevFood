967cd31e2ac9d391c4aad218e32a80fdf8e75d46
dicutrim <diegocutrimcontato@gmail.com>
2025-05-17 00:36:01 -0300

    Implementação da SecurityConfiguration, do USerDetailsService e finalização do método de registrar usuários

--- src/main/java/com/domain/devfood/Service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/domain/devfood/Service/impl/UserDetailsServiceImpl.java
@@ -1,4 +1,23 @@
 package com.domain.devfood.Service.impl;
 
-public class UserDetailsServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import com.domain.devfood.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("User not foound")
+        );
+    }
 }
--- src/main/java/com/domain/devfood/controller/UserController.java
+++ src/main/java/com/domain/devfood/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.domain.devfood.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import com.domain.devfood.mapper.UserMapper;
+import com.domain.devfood.Service.UserService;
+import org.springframework.http.ResponseEntity;
+import com.domain.devfood.dto.RequestRegisterUser;
+import com.domain.devfood.dto.ResponseRegisteredUser;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping
+    public ResponseEntity<ResponseRegisteredUser> register(@RequestBody RequestRegisterUser request) {
+        var encryptedPassword = passwordEncoder.encode(request.getPassword());
+        request.setPassword(encryptedPassword);
+
+        var registeredUser = userService.registerUser(UserMapper.toUser(request));
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(UserMapper.toResponseRegisteredUser(registeredUser));
+    }
 }
